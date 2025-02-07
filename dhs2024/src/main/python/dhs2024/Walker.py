2ed75212e8f37a5621daf29c0666354a20382a43
joaquincernik <joaquincernik@gmail.com>
2025-02-07 19:49:20 -0300

    hasta aca identifica las multiplicaciones y divisiones

--- dhs2024/src/main/python/dhs2024/Walker.py
+++ dhs2024/src/main/python/dhs2024/Walker.py
@@ -3,27 +3,48 @@ from compiladoresParser import compiladoresParser
 
 class Walker (compiladoresVisitor):
 
+    contadorVarTemporales = 0
+    variablesTemporales = []
 
     def visitPrograma(self, ctx: compiladoresParser.ProgramaContext):
         print("=" *20)
         print("comienza a caminar")
         return super().visitPrograma(ctx)
     
-    def visitAsignacion(self, ctx: compiladoresParser.AsignacionContext):
-        print (ctx.getChild(0).getText() + " - " + ctx.getChild(1).getText())
-        return None
+    #def visitAsignacion(self, ctx: compiladoresParser.AsignacionContext):
+     #   print (ctx.getChild(0).getText() + " - " + ctx.getChild(1).getText())
+      #  return None
 
     # def visitAsign (self, ctx: compiladoresParser.DeclaracionContext):
     #     print (ctx.getChild(0).getText() + " - " + ctx.getChild(1).getText())
     #     return None
     
+    def visitTermino5(self, ctx):
+        print("t"+str(self.contadorVarTemporales) + "="+ctx.getText())
+        self.variablesTemporales.append(self.contadorVarTemporales)
+        self.contadorVarTemporales = self.contadorVarTemporales + 1
+        #return super().visitTermino5(ctx)
+
+    def visitTermino4(self, ctx):
+        #print(ctx.getText())
+        return super().visitTermino4(ctx)
+      
+   
+    """ def visitPartemuldivmod(self, ctx):
+        print("Aca se esta multiplicando/dividiendo " + ctx.getText())
+        return super().visitPartemuldivmod(ctx)
+
+    def visitPartesumaresta(self, ctx):
+        print("Aca se esta sumando/restando " + ctx.getText())
+        return super().visitPartesumaresta(ctx)
+     
     def visitBloque(self, ctx: compiladoresParser.BloqueContext):
         print("nuevo contexto")
         print(ctx.getText())
         return super().visitInstrucciones(ctx.getChild(1))
-
-    def visitTerminal(self, node):
-        print("Token " + node.getText())
-        return super().visitTerminal(node)
+        """
+    # def visitTerminal(self, node):
+    #     print("Token " + node.getText())
+    #     #return super().visitTerminal(node)
     
     
